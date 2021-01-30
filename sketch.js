85f73e75dd1b21e212435a2338270964a5c8ef34
anjali-2008 <76835103+anjali-2008@users.noreply.github.com>
2021-01-30 10:26:48 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,12 +5,12 @@ var jerry, jerryImg1,jerryImg2;
 
 function preload() {
     bg = loadImage("images/garden.png");
-    tomImg1= loadAnimation("images/tomOne.png");
+    tomImg1= loadImage("images/tomOne.png");
     tomImg2=loadAnimation("images/tomTwo.png","images/tomThree.png");
-    tomImg3= loadAnimation("images/tomFour.png");
-    jerryImg1=loadAnimation("images/jerryOne.png");
+    tomImg3= loadImage("images/tomFour.png");
+    jerryImg1=loadImage("images/jerryOne.png");
     jerryImg2= loadAnimation("images/jerryTwo.png","images/jerryThree.png");
-    jerryImg3=loadAnimation("images/jerryFour.png");
+    jerryImg3=loadImage("images/jerryFour.png");
 
 }
 
@@ -18,11 +18,11 @@ function setup(){
     canvas = createCanvas(1000,800);
 
     tom = createSprite(870, 600);
-    tom.addAnimation("tomInitialPosition", tomImg1);
+    tom.addImage("tomInitialPosition", tomImg1);
     tom.scale = 0.2;
 
     jerry = createSprite(200, 600);
-    jerry.addAnimation("jerryInitialPosition", jerryImg1);
+    jerry.addImage("jerryInitialPosition", jerryImg1);
     jerry.scale = 0.15;
 
 }
@@ -34,13 +34,13 @@ function draw() {
     if(tom.x - jerry.x < (tom.width - jerry.width)/2)
     { 
         tom.velocityX=0;
-        tom.addAnimation("tomLastImage", tomImg3);
+        tom.addImage("tomLastImage", tomImg3);
         tom.x =300;
         tom.scale=0.2;
-        tom.changeAnimation("tomLastImage");
-        jerry.addAnimation("jerryLastImage", jerryImg3);
+      //  tom.changeAnimation("tomLastImage");
+        jerry.addImage("jerryLastImage", jerryImg3);
         jerry.scale=0.15;
-        jerry.changeAnimation("jerryLastImage");
+      // jerry.changeAnimation("jerryLastImage");
     }  
 
     drawSprites();
@@ -52,10 +52,10 @@ function keyPressed(){
     if(keyCode === LEFT_ARROW){
         tom.velocityX = -5; 
         tom.addAnimation("tomRunning", tomImg2);
-        tom.changeAnimation("tomRunning");
+       // tom.changeAnimation("tomRunning");
         
         jerry.addAnimation("jerryTeasing", jerryImg2);
         jerry.frameDelay = 25;
-        jerry.changeAnimation("jerryTeasing");
+      //  jerry.changeAnimation("jerryTeasing");
     }
 }
